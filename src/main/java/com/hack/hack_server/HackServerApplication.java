6c09c6a1b995c48a406cf72f3ab1f4c5541b0c77
하윤지 <hayunji@hayunjiui-MacBookPro.local>
2023-07-10 16:24:51 +0900

    git ignore 설정

---
+++ src/main/java/com/hack/hack_server/HackServerApplication.java
@@ -0,0 +1,13 @@
+package com.hack.hack_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hack/hack_server/HackServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hack.hack_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
