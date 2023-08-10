18258e524001e39825a3e0f2c2c1e74215968a7f
yunji118 <yunji118@naver.com>
2023-08-10 15:11:15 +0900

    [feat]character테이블수정 #19

--- src/main/java/com/hack/hack_server/Entity/Character.java
+++ src/main/java/com/hack/hack_server/Entity/Character.java
@@ -1,2 +1,20 @@
-package com.hack.hack_server.Entity;public class Character {
+package com.hack.hack_server.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+public class Character extends BaseTimeEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "character_id")
+    private Long id;
+
+    private String type;
 }
