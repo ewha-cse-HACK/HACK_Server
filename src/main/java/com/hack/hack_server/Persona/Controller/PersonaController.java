2544ad09c0f154f66e3dabbd3e601da7642c9eef
yunji118 <yunji118@naver.com>
2023-08-14 15:23:27 +0900

    [feat]종 추가 후 저장

--- src/main/java/com/hack/hack_server/Persona/Controller/PersonaController.java
+++ src/main/java/com/hack/hack_server/Persona/Controller/PersonaController.java
@@ -1,9 +1,11 @@
 package com.hack.hack_server.Persona.Controller;
 
+import com.hack.hack_server.Authentication.PrincipalDetails;
 import com.hack.hack_server.Persona.Dto.SpeciesRequestDto;
 import com.hack.hack_server.Persona.Service.PersonaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +18,13 @@ public class PersonaController {
     private final PersonaService personaService;
 
     @PostMapping("/species")
-    public Long saveSpecies(@RequestBody SpeciesRequestDto requestDto){
-        return personaService.saveSpecies(requestDto);
+    public Long saveSpecies(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody SpeciesRequestDto requestDto){
+        return personaService.saveSpecies(principalDetails, requestDto);
     }
 
     @PostMapping("/species/new")
-    public ResponseEntity addNewSpecies(@RequestBody SpeciesRequestDto requestDto){
-        return personaService.addSpecies(requestDto);
+    public ResponseEntity addNewSpecies(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody SpeciesRequestDto requestDto){
+        return personaService.addSpecies(principalDetails, requestDto);
     }
 
 }
