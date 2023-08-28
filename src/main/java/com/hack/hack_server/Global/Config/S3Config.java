75f5a5d754e40f2f544cbeefc082f0f14a798940
yunji118 <yunji118@naver.com>
2023-08-28 17:19:49 +0900

    [bug]amazonS3client 오류 #39

--- src/main/java/com/hack/hack_server/Global/Config/S3Config.java
+++ src/main/java/com/hack/hack_server/Global/Config/S3Config.java
@@ -6,6 +6,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,14 +23,21 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Bean
+    @Autowired
     public AmazonS3 amazonS3Client(){
-        BasicAWSCredentials awsCredentials= new BasicAWSCredentials(accessKey, secretKey);
+//        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+//
+//        return (AmazonS3Client) AmazonS3ClientBuilder
+//                .standard()
+//                .withRegion(region)
+//                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+//                .build();
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
-        return (AmazonS3Client) AmazonS3ClientBuilder
+        return AmazonS3ClientBuilder
                 .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
 
