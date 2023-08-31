bab2d1edab7c2f13e8fe1eed30d06eced901173e
Minji Kim <minji1289@naver.com>
2023-08-31 16:30:26 +0900

    [feat] GPT가 생성한 일기 DB에 저장

---
+++ src/main/java/com/hack/hack_server/Entity/Journal.java
@@ -0,0 +1,44 @@
+package com.hack.hack_server.Entity;
+
+import com.hack.hack_server.Persona.Dto.PetRequestDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@DynamicInsert
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+public class Journal extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "journal_id")
+    private Long id;
+
+    //그림 일기
+    private String image;
+
+    //일기 내용
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", referencedColumnName = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pet_id", referencedColumnName = "pet_id")
+    private Pet pet;
+
+    @Builder
+    public Journal(User user, Pet pet, String image, String content){
+        this.user = user;
+        this.pet = pet;
+        this.image = image;
+        this.content = content;
+    }
+}
