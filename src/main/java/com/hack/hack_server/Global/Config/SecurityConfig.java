4d5085a28bb64919da9848afdb21a0bc14f6152f
Minji Kim <minji1289@naver.com>
2023-09-26 17:13:03 +0900

    [feat] CORS 설정

--- src/main/java/com/hack/hack_server/Global/Config/SecurityConfig.java
+++ src/main/java/com/hack/hack_server/Global/Config/SecurityConfig.java
@@ -16,6 +16,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.time.Duration;
 
 @RequiredArgsConstructor
 @Configuration
@@ -55,6 +60,21 @@ public class SecurityConfig {
                         .anyRequest().permitAll()	// 어떠한 요청이라도 인증필요
                 );
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("https://wowmarket-web.vercel.app");
+        configuration.addAllowedOrigin("https://wowmarket-web.vercel.app/");
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(Duration.ofSeconds(3600));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
