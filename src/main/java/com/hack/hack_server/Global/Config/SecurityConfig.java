de5f14bee18d7e70b4d5b40d497abec46d1f63b8
Minji Kim <minji1289@naver.com>
2023-09-26 17:30:58 +0900

    [fix] CORS

--- src/main/java/com/hack/hack_server/Global/Config/SecurityConfig.java
+++ src/main/java/com/hack/hack_server/Global/Config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(formLogin -> formLogin.disable())
