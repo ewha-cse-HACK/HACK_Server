162c613f3fb50ea21b877f30e44d39f67802ab3f
yunji118 <yunji118@naver.com>
2023-11-23 01:28:52 +0900

    [demo] gpt 3.5 turbo

--- src/main/java/com/hack/hack_server/ChatGpt/ChatGptConfig.java
+++ src/main/java/com/hack/hack_server/ChatGpt/ChatGptConfig.java
@@ -3,7 +3,7 @@ package com.hack.hack_server.ChatGpt;
 public class ChatGptConfig {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String MODEL = "gpt-4-1106-preview";
+    public static final String MODEL = "gpt-3.5-turbo";
     public static final Integer MAX_TOKEN = 100;
     public static final Double TEMPERATURE = 0.5;
     public static final Double TOP_P = 1.0;
